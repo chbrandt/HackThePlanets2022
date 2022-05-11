466e57b110e5a97dcb1e520a36ef32676123795f
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-11 13:07:48 +0200

    Add notebook, update README

---
+++ adamapi_docker/README.md
@@ -0,0 +1,14 @@
+# Docker image for the adam api service
+
+This docker image provides an Ubuntu container with python 3.8 and the adamapi Application Processing Interfaces python library.
+
+## Using the image
+Build the image with:
+```
+cd adamapi_docker
+docker build --tag latest .
+```
+Start a jupyter server inside a new container:
+```
+./start_notebook.sh
+```
--- adamapi_docker/start_notebook.sh
+++ adamapi_docker/start_notebook.sh
@@ -1,8 +1,8 @@
  #!/bin/bash 
 
- docker run --rm -it -p 8888:8888 \
+docker run --rm -it -p 8888:8888 \
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 -v $PWD/shared_dir:/shared_dir \
-adamapiimg:v0.0.2 /bin/bash -c \
-"source /opt/venv/adamapienv/bin/activate && jupyter notebook --ip='*' --port=8888 --no-browser --allow-root --notebook-dir='/app' --NotebookApp.token='' --NotebookApp.password=''"
+adamapiimg:latest /bin/bash -c \
+"source /opt/venv/adamapienv/bin/activate && jupyter notebook --ip='*' --port=8888 --no-browser --allow-root --notebook-dir='/shared_dir' --NotebookApp.token='' --NotebookApp.password=''"
