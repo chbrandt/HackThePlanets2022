de78850fcc3bf7595b5e1ae03ac991acc4457497
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-16 17:22:39 +0200

    Add pyproect and setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("requirements.lock", "r", encoding="utf-8") as fh:
+   requirements = [d.strip() for d in fh.readlines() if "#" not in d]
+
+setuptools.setup( 
+     name='hacktheplanets2022',
+     author='Addis Antonio, Baroncelli Leonardo, Di Piano Ambra',
+     author_email='antonio.addis@inaf.it, leonardo.baroncelli@inaf.it, ambra.dipiano@inaf.it',
+     package_dir={'hackp': 'hackp'},
+     license='BSD-3-Clause',
+     python_requires=">=3.8.10",
+     install_requirements=[
+          requirements
+     ]
+)
