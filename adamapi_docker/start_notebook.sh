ca51c9fc9aadfa5b3d61f6d27f9d8ded00950f95
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-16 17:13:04 +0200

    Move notebook folder

--- adamapi_docker/start_notebook.sh
+++ adamapi_docker/start_notebook.sh
@@ -1,5 +1,7 @@
  #!/bin/bash 
 
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if [[ -z "${SHARED_VOLUME}" ]]; then
     printf "\n\n\t> Please export an environment variable called 'SHARED_VOLUME' to run the container!\n\n"
 else
@@ -7,7 +9,7 @@ else
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v "${SHARED_VOLUME}":/shared_volume \
-    -v $PWD/notebooks:/shared_dir \
+    -v $script_dir/../notebooks:/shared_dir \
     adamapiimg:latest /bin/bash -c \
     "source /opt/venv/adamapienv/bin/activate && jupyter notebook --ip='*' --port=8888 --no-browser --allow-root --notebook-dir='/shared_dir' --NotebookApp.token='' --NotebookApp.password=''"
 fi
