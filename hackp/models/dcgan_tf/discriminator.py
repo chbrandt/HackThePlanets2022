6cb92a0567614cadbb5a77bd28f69f0cce2acf84
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-18 06:49:27 +0200

    simple gan

---
+++ hackp/models/dcgan_tf/discriminator.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def make_discriminator_model(image_size):
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[image_size, image_size, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    #model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    #model.add(layers.LeakyReLU())
+    #model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+
+def discriminator_loss(real_output, fake_output):
+    # This method returns a helper function to compute cross entropy loss
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
