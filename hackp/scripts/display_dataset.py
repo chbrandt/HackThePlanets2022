a0f30c1ba4aa9322d09e52c1c938e6255c9268da
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-19 10:22:08 +0200

    Add argparse

--- hackp/scripts/display_dataset.py
+++ hackp/scripts/display_dataset.py
@@ -1,20 +1,25 @@
-import numpy as np
+import argparse
 import matplotlib
+import numpy as np
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 
 if __name__=='__main__':
 
-    regions = np.load("/shared_volume/npy/F16_042066_1532_XN_26S322W.cal.npy")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-if", "--input-file", type=str, required=True, help='The path to an input file in .npy format')
+    parser.add_argument("-sz", "--size", type=int, required=True, help='The size of the input square image')
+    parser.add_argument("-hm", "--how-many", type=int, required=True, help='How many .png files will be produced')
+    args = parser.parse_args()
 
-    size = 1024
+    regions = np.load(args.input_file)
 
     print(f"Regions shape: {regions.shape}")
 
-    for i, region in enumerate(regions[0:20]):
+    for i, region in enumerate(regions[0:args.how_many]):
         plt.figure()
         ax = plt.subplot()
-        ax.imshow(region.reshape(size,size), cmap="magma")
-        plt.savefig(f"{i}.png")
+        ax.imshow(region.reshape(args.size,args.size), cmap="magma")
+        plt.savefig(f"{args.size}-{i}.png", dpi=500)
     
