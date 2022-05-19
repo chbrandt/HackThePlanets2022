91a9dab7b9b318f517618373898298f5509f32e8
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-19 09:50:07 +0200

    Fix bug

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("requirements.lock", "r", encoding="utf-8") as fh:
-   requirements = [d.strip() for d in fh.readlines() if "#" not in d]
+#with open("requirements.lock", "r", encoding="utf-8") as fh:
+#   requirements = [d.strip() for d in fh.readlines() if "#" not in d]
 
 setuptools.setup( 
      name='hacktheplanets2022',
