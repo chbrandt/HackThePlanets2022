b2e8df977706d70dcc3a68123dd8684ecd77a649
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-05-30 19:09:29 +0200

    Single process

--- hackp/scripts/create_dataset.py
+++ hackp/scripts/create_dataset.py
@@ -3,47 +3,40 @@ import logging
 import argparse
 import numpy as np
 from time import time
-from tqdm import tqdm
 from os import listdir
 import multiprocessing
 from pathlib import Path
 from functools import partial 
-#from collections import defaultdict
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from hackp.utils.tif import read_tif
 from hackp.utils.imgproc import extract_regions
 
-
-def save_img(img, output_dir, format, filename):
+def save_img(img, output_dir, filename):
 
     output_file = output_dir.joinpath(filename).with_suffix(".npy")
     
     if not output_file.exists():
-
-        if format == "npy":
-            # print(f"Saving {output_file}")
-            np.save(output_file, img)
-            return True
-
-        elif format == "png":
-            return True
+        np.save(output_file, img)
     
     return False 
     
-def extract_patch_and_save(output_dir, format, tif_file):
+def extract_patch_and_save(output_dir, tif_file):
 
         pid = multiprocessing.current_process().name
 
-        logging.info(f"{pid} - Opening: {tif_file.stem}")
+        logging.info(f"{pid} - Opening and extracting images from: {tif_file.stem}")
 
         start_t = time()
 
-        regions = np.expand_dims( 
-                        extract_regions(read_tif(tif_file), args.size, args.stride)
-                            .reshape(-1, args.size, args.size)
-        , axis=-1) # => (N,H,W,C)
- 
+        try:
+            regions = np.expand_dims( 
+                            extract_regions(read_tif(tif_file), args.size, args.stride)
+                                .reshape(-1, args.size, args.size)
+            , axis=-1) # => (N,H,W,C)
+        except Exception as e:    
+            logging.error(f"{pid} - Exception during reading tif and images extraction:\n\n{e}")
+            return 0
 
         tf_time = time() - start_t
         logging.info(f"{pid} - Extracted images: {regions.shape}. Took {round(tf_time,2)}s. Writing the images on disk..") # (N, H, W)
@@ -51,7 +44,7 @@ def extract_patch_and_save(output_dir, format, tif_file):
         start_t = time()
         count = 0
         for idx,region in enumerate(regions):
-            if save_img(region, output_dir, format, tif_file.stem+f"_{idx}"):
+            if save_img(region, output_dir, tif_file.stem+f"_{idx}"):
                 count += 1
 
         w_time = time() - start_t
@@ -68,7 +61,6 @@ if __name__=='__main__':
     parser.add_argument("-o", "--output", type=str, required=True, help='The path to an output direcotry that will contain the output files')
     parser.add_argument("-sz", "--size", type=int, required=True, help='The size of the square images that are going to be extracted')
     parser.add_argument("-st", "--stride", type=int, required=True, help='If stride==size, there will be no overlapping')
-    parser.add_argument("-f", "--format", type=str, required=True, choices=["npy", "png"], help="The output file format")
     parser.add_argument("-j", "--jobs", type=int, required=False, default=2, help="The number of worker reader threads")
     args = parser.parse_args()
 
@@ -96,12 +88,16 @@ if __name__=='__main__':
 
     unzipped_data_files = [dataset.joinpath(f) for f in listdir(dataset) if dataset.joinpath(f).exists()]
 
-    results = []
-    with multiprocessing.Pool(processes=args.jobs) as pool:
 
-        for result in tqdm(pool.imap_unordered(partial(extract_patch_and_save, output_dir, args.format), unzipped_data_files), total=len(unzipped_data_files)):
-            results.append(result)
-        pool.close()
-        pool.join()
+    if args.jobs > 0:
+        results = []
+        with multiprocessing.Pool(processes=args.jobs) as pool:
+            for result in pool.imap(partial(extract_patch_and_save, output_dir), unzipped_data_files):
+                results.append(result)
+            pool.close()
+            pool.join()
+        print(f"Total files produced: {sum(results)}")
 
-    print(f"Total files produced: {sum(results)}")
+    else:
+        for unzipped_data_file in unzipped_data_files:
+            extract_patch_and_save(output_dir, unzipped_data_file)
