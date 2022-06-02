621d0a99e84816ce21160f88356c29f7926fc960
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-06-02 14:58:48 +0200

    Add time benchmark

--- hackp/scripts/inspect_npy.py
+++ hackp/scripts/inspect_npy.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+from time import time
 
 if __name__=='__main__':
 
@@ -7,6 +8,8 @@ if __name__=='__main__':
     parser.add_argument("-f", "--file", type=str, required=True, help='The path to an input npy file')
     args = parser.parse_args()
 
+    s = time()
     data = np.load(args.file)
+    print(f"Loading took {time()-s} s")
     print(data)
     print(data.shape)
