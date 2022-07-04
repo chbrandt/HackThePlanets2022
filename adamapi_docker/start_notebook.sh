85ae7fa18ba9b5983ef0f37a3aa03342674c7c8a
Carlos H Brandt <c.brandt@jacobs-university.de>
2022-07-04 22:40:04 +0200

    Remove lse volume, Add

--- adamapi_docker/start_notebook.sh
+++ adamapi_docker/start_notebook.sh
@@ -1,18 +1,19 @@
- #!/bin/bash 
+#!/usr/bin/env bash 
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+NBPORT=8881
 
 if [[ -z "${SHARED_VOLUME}" ]]; then
     printf "\n\n\t> Please export an environment variable called 'SHARED_VOLUME' to run the container!\n\n"
 else
-    docker run --rm -it -p 8888:8888 \
+    docker run --rm -it -p $NBPORT:$NBPORT \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v "${SHARED_VOLUME}":/shared_volume \
-#    -v /data01/homes/baroncelli/HackThePlanets2022/nenias-latent-space-explorer-generators/inference/20220623-174308-simclr_oasfbo:/output \
     -v $script_dir/../notebooks:/shared_dir/notebooks \
     -v $script_dir/../data:/shared_dir/data \
     -v $script_dir/../hackp:/shared_dir/hackp \
     adamapiimg:latest /bin/bash -c \
-    "source /opt/venv/adamapienv/bin/activate && jupyter notebook --ip='*' --port=8888 --no-browser --allow-root --notebook-dir='/shared_dir/notebooks' --NotebookApp.token='' --NotebookApp.password=''"
+    "source /opt/venv/adamapienv/bin/activate && jupyter notebook --ip='*' --port=$NBPORT --no-browser --allow-root --notebook-dir='/shared_dir/notebooks' --NotebookApp.token='' --NotebookApp.password=''"
 fi
